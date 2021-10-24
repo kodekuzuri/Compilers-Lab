046f58e8f24a59de03e8f162a2dd8ae69588fa94
Parth Jindal <pmjindal@gmail.com>
2021-10-24 18:03:45 +0530

    update translators

--- Assignment5/assgn5_19CS30033_19CS30036_translator.h
+++ Assignment5/assgn5_19CS30033_19CS30036_translator.h
@@ -9,10 +9,11 @@ ASSIGNMENT: 5
 
 // including header files needed
 #include <iostream>
-#include <vector>
-#include <string>
 #include <list>
 #include <map>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 extern char* yytext;
@@ -22,213 +23,193 @@ extern int yyparse();
 #define size_of_char 1
 #define size_of_int 4
 #define size_of_float 8
-#define size_of_int_pointer 4
-#define size_of_float_pointer 4
-#define size_of_char_pointer 4
+#define size_of_ptr 4
 
 //sizes are defined as constants as hardcoded values make the program machine-dependent
 //these values can be changed according to the machine it is being executed on
 
 //CLASS DECLARATIONS
-class symbol ;
-class quad ;
-class symType ;
-class symTable ;
-class quadArray ;
+class symbol;
+class quad;
+class label;
+class symType;
+class symTable;
+class quadArray;
+
+struct expression;
+struct statement;
+struct Array;
 
 // Pointer to current symbol
-extern symbol* currS ;
-
-// current symbol table for the code 
-extern symTable STable ;
-
+extern symbol* currS;
+// current symbol table for the code
+extern symTable* ST;
 // global Symbol Table
-extern symTable globalSTable ;
-
+extern symTable* globalST;
 // array of quads for the ones in the code
-extern quadArray QArray ;
+extern quadArray QArray;
+extern std::string varType;
+extern std::string loopName;
+extern symTable* parST;
+extern long long int tableCount;
+extern std::vector<label> labelList;
 
 //CLASS DEFINITIONS
 
 //-- CLASS SYMBOL
+class symbol {
+   public:
+    std::string name;    // name of the symbol
+    symType* type;       // type of the symbol
+    string val;          // initial value
+    int size;            // size of the symbol
+    int offset;          // offset of the symbol in ST
+    symTable* nestedST;  // ptr to nested symbol table
 
-class symbol{
-    public:
-
-    // name of the symbol
-    string name ;
-
-    // type of symbol
-    symType* s_type ;
-
-    // to store any initial value present for the symbol
-    string iVal ;
-
-    // storing size of the symbol
-    int size ;
-
-    // storing the offset from the base pointer
-    int offset ;
-
-    // This pointer points to the nest 
-    symTable* nestedST ;
-
-    //member functions
-    
-    //Class constructor for initizalization 
-    symbol(string sname, string tt = "INT", symType* p = NULL, int ss = 0) ;
-
-    //function to update any symbol
-    symbol* Update(symType* ts) ;
+    /******CONSTRUCTOR********/
+    symbol(string name_, string type_ = "int",
+           symType* ptr = NULL,
+           int width = 0);
+    symbol* update(symType* ts);
 };
+
+class label {
+   public:
+    string name;
+    int addr;
+    std::list<int> nextList;
+    label(string name_, int addr_ = -1);
+};
+
+//TODO: add a constructor for label
 
 //--CLASS QUAD
-
-class quad{
-public:
-    // res = arg2 op arg1
-    string res ;
-    string arg1 ;
-    string op ;
-    string arg2 ;
+class quad {
+   public:
+    // res = arg1 op arg2
+    std::string res;
+    std::string arg1;
+    std::string op;
+    std::string arg2;
 
     // function to print the current quad
-    void PRINT() ;
+    void print();
 
     // Constructors for quad initialization
-    quad(string res, string arg1, string op = "=", string arg2 = "");
-    quad(string res, float arg1, string op = "=", string arg2 = "");
-    quad(string res, int arg1, string op = "=", string arg2 = "");
+    quad(std::string res, std::string arg1, std::string op = "=", std::string arg2 = "");
 };
-
 
 //--CLASS SYMBO TYPW
 
-class symType{
-public:
-    // string to store symbol type
-    string type ;
-    // pointer to elements in case of complex symbol type such as array
-    symType* p ;
-    // number of elements.
-    // for arrays etc. width = n
-    // for simple width = 1
-    int width ;
+class symType {
+   public:
+    std::string type;  // string to store symbol type
+    symType* ptr;      // pointer to elements in case of complex symbol type such as array
+    int width;         // width if arrType
 
-    // constructor for intialization
-    symType(string type, symType* p = NULL, int width = 1);
+    /******CONSTRUCTOR********/
+    symType(std::string type, symType* ptr = NULL, int width = 1);
 };
 
 // --CLASS QUAD ARRAY
 
-class quadArray{
-public:
+class quadArray {
+   public:
     // vector to store all quads in code
-    vector<quad> quadA ;
-
+    std::vector<quad> array;
     // member function to print all the quads
     void print();
 };
 
 //--CLASS SYMTABLE
-
-class symTable{
-public:
-    // variable to store the name of the table
-    string name ;
-    // counter for the number of temporary variables used
-    int counter ;
-    // list of symbols to store symbol table
-    list<symbol> ST ;
-    // pointer to store parent symbol table
-    symTable* parent
+class symTable {
+   public:
+    std::string name;         // name of the symbol table
+    int count;                // counter for temp variables
+    std::list<symbol> table;  // list of symbols in the table
+    symTable* parent;         // ptr to parent symbol table
 
     //member functions
-    symTable(string name = "NULL");
+    symTable(std::string name_ = "NULL");
+    ~symTable() {}
 
-    // function to update the symbol table
-    void update();
-
-    //function to print the symbol table
-    void print();
-
-    //function that searches for a symbol by name and returns a pointer to it
-    symbol* lookup(string name);
+    void update();                         // update the symbol table
+    void print();                          // print the symbol table
+    symbol* lookup(std::string symbName);  // lookup a symbol in the symbol table and return prt to it
 };
 
-struct statement{
-    // list to store 
-    list<int> nextList ;
+struct statement {
+    // list to store statements for dangling exits
+    std::list<int> nextList;
 };
 
-struct expression{
+struct expression {
     // string to store type
-    string type ;
+    std::string type;
     // pointer to the entry in symbol table
-    symbol* loc ;
+    symbol* loc;
     // next list for statement expressions
-    list<symbol> nextList ;
+    std::list<int> nextList;
     // false list for boolean expressions
-    list<symbol> falseList ;
+    std::list<int> falseList;
     // true list for boolean expressions
-    list<symbol> trueList ;
+    std::list<int> trueList;
 };
 
 // emit function used by the parser in overloaded state
-void emit(string res, string op, string arg1 = "", string arg2 = "");
-void emit(string res, string op, int arg1 = "", string arg2 = "");
-void emit(string res, string op, float arg1 = "", string arg2 = "");
+void emit(string op, string res, string arg1 = "", string arg2 = "");
+void emit(string op, string res, int arg1, string arg2 = "");
+void emit(string op, string res, float arg1, string arg2 = "");
 
-struct array{
+//GENTEMP FUNCTION
+symbol* gentemp(symType* T, string initVal = "");
+
+// inserting target label into group of quads i.e. list<int> L
+void backpatch(list<int>& L, int i);
+// making a new list with only the integer address passed as parameter
+list<int> makelist(int i);
+// function to merge the 2 lists L1 and L2 passed as parameters
+list<int> merge(list<int>& L1, list<int>& L2);
+
+struct Array {
     // string to store type of array [pointers , elements]
-    string artype ;
+    string artype;
     // symbol type of generated subarray stored
     // important for when multidimensional array in encountered
-    symType* type ;
+    symType* type;
     // pointer to store location of array
-    symbol* loc ;
+    symbol* loc;
     // pointer to symbol table entry
-    symbol* Sarr ;
-} ;
+    symbol* Sarr;
+};
 
 // LIST RELATED FUNCTIONS
-
-// making a new list with only the integer address passed as parameter 
-list<int> makelist (int i);      
-// function to merge the 2 lists L1 and L2 passed as parameters
-list<int> merge (list<int> &L1, list <int> &L2) ; 
-// inserting target label into group of quads i.e. list<int> L
-void backpatch (list <int> L, int i);                         
 
 // BOOLEAN RELATED FUNCTIONS
 
 // function for conversion of boolean expression to int
-expression* convertBool2Int(expression*) ;
+expression* convertBool2Int(expression*);
 // function for conversion of integer expression to int
-expression* convertInt2Bool(expression*) ;
+expression* convertInt2Bool(expression*);
 
 // overloaded function to check if the symbol type is same for 2 symbols
-bool checkType(symbol* &c1, symbol* &c2) ;
-bool checkType(symType* s1, symType* s2) ;
+bool checkType(symbol*& c1, symbol*& c2);
+bool checkType(symType* s1, symType* s2);
 
 // function to get the size of the symbol
-int typeSize(symType*) ;
+int typeSize(symType*);
 
 // function to get the type of the symbol
-string typeGet(symType*) ;
-
-
-//GENTEMP FUNCTION
-symbol* gentemp(symTable* T, string initVal = "") ;
+string typeGet(symType*);
 
 // returns the number of the next instruction
-int nextInstruction() ;
+int nextInstruction();
 
-// global function for the conversion of symbol to the type stored in string  
-symbol* convert(symbol* , string) ;
+// global function for the conversion of symbol to the type stored in string
+symbol* convert(symbol*, string);
 
 // changing table passed as paramater to a new symbol table
-void changeTable(symTable*) ;                                                  
+void changeTable(symTable*);
+label* findLabel(string name);
 
-#endif // __TRANSLATION_H
-
+#endif  // __TRANSLATION_H
