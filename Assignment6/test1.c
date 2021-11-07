c889dad75ee38c41dc6f858a4c61236d7ace3630
Parth Jindal <pmjindal@gmail.com>
2021-11-07 21:38:13 +0530

    add all files

--- Assignment6/test1.c
+++ Assignment6/test1.c
@@ -14,27 +14,37 @@ int main() {
     int a, b;
     int e;
 
-    printStr("        #########################################################\n        ##                 TESTING FUNCTIONS                   ##\n        #########################################################        \n");
-
-    b = 3;
-
-    // checking printStr() and printStr()
-    printStr("\n\n        Passing parameter to function :\n        int test(int *a)    \n");
-    printStr("\n        Value passed to function: ");
-    printInt(b);
-    printStr("\n");
-
-    a = test(b);
-    printStr("\n        Address returned from function is: ");
-    printInt(a);
-    printStr("\n");
-
-    printStr("\n        #####################################################\n        ##              READ AN INTEGER                    ##\n        ##                TESTING I/O                      ##\n        #####################################################\n        \n");
-    printStr("\nEnter an Integer : ");
+    a = readInt(&e);
     b = readInt(&e);
-    printStr("The integer that was read is : ");
-    printInt(b);
-    printStr("\n");
+
+    if (a == b) {
+        printStr("a and b are equal");
+    } else if (a < b) {
+        printStr("a  < b");
+    } else
+        printStr("a  > b");
+
+    // printStr("        #########################################################\n        ##                 TESTING FUNCTIONS                   ##\n        #########################################################        \n");
+
+    // b = 3;
+
+    // // checking printStr() and printStr()
+    // printStr("\n\n        Passing parameter to function :\n        int test(int *a)    \n");
+    // printStr("\n        Value passed to function: ");
+    // printInt(b);
+    // printStr("\n");
+
+    // a = test(b);
+    // printStr("\n        Address returned from function is: ");
+    // printInt(a);
+    // printStr("\n");
+
+    // printStr("\n        #####################################################\n        ##              READ AN INTEGER                    ##\n        ##                TESTING I/O                      ##\n        #####################################################\n        \n");
+    // printStr("\nEnter an Integer : ");
+    // b = readInt(&e);
+    // printStr("The integer that was read is : ");
+    // printInt(b);
+    // printStr("\n");
 
     return 0;
 }
